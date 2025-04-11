53e6da059a9a5789a01ba4ddf7a0243618ed153c
Macey Ragbir <maceyragbir@gmail.com>
2025-04-11 01:22:01 -0400

    Unfinished Firebase
    In process of setting up firebase

---
+++ public/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDobzEi69_6fx6VsttSEcJCNxTt1-HBEEU",
+    authDomain: "codetrekkerapp.firebaseapp.com",
+    projectId: "codetrekkerapp",
+    storageBucket: "codetrekkerapp.firebasestorage.app",
+    messagingSenderId: "846059095122",
+    appId: "1:846059095122:web:ccb769e315f60aac130b8b",
+    measurementId: "G-TNZYWKBQJE"
+};
+
+export default firebaseConfig
